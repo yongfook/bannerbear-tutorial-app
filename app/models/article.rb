139357e8a59bd5cbc4cec28652cd52891c025d7a
Jon Cockle <yongfook@gmail.com>
2020-03-30 21:32:01 +0800

    validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+	validates_uniqueness_of :url
+	validates :url, format: URI::regexp(%w[http https])
+	validate :is_wikipedia_url
+
+	def is_wikipedia_url
+		uri = URI.parse(url.downcase)
+		if uri.host
+			return true if uri.host.match /[a-z]{2}\.wikipedia\.org/
+			errors.add(:url, "must be an article on wikipedia.org")
+		end
+	end
+
 end
