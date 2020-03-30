3db21a1e76418adfaf355f75964640913d8926b0
Jon Cockle <yongfook@gmail.com>
2020-03-30 20:49:57 +0800

    added form

--- Gemfile
+++ Gemfile
@@ -54,4 +54,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "slim-rails"
-
+gem "bulma-rails"
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.fetch(:article, {})
+      params.fetch(:article, {}).permit(:url)
     end
 end
